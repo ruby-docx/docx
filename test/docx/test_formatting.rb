527deb0c9db0a0b3912c0613203c0cc527c56a0a
Marcus Ortiz <mportiz08@gmail.com>
2012-04-11 15:19:37 -0700

    testing mixed formatting and multiple text runs

--- test/docx/test_formatting.rb
+++ test/docx/test_formatting.rb
@@ -4,33 +4,49 @@ require 'test/unit'
 class FormattingTest < Test::Unit::TestCase
   def setup
     @doc = Docx::Document.open('test/fixtures/formatting.docx')
-    @formatting = @doc.paragraphs.map { |p| p.text_runs.first.formatting }
+    @formatting = @doc.paragraphs.map { |p| p.text_runs.map(&:formatting) }
   end
   
   def test_document_text
-    assert_equal 5,           @doc.paragraphs.size
+    assert_equal 6,           @doc.paragraphs.size
     assert_equal 'Normal',    @doc.paragraphs[0].text
     assert_equal 'Italic',    @doc.paragraphs[1].text
     assert_equal 'Bold',      @doc.paragraphs[2].text
     assert_equal 'Underline', @doc.paragraphs[3].text
-    assert_equal 'normal',    @doc.paragraphs[4].text
+    assert_equal 'Normal',    @doc.paragraphs[4].text
+    assert_equal 'This is a sentence with all formatting options in the middle of the sentence.',
+                              @doc.paragraphs[5].text
+  end
+  
+  def test_multiple_text_runs_in_paragraph
+    p = @doc.paragraphs[5]
+    assert_equal 3, p.text_runs.size
+    assert_equal 'This is a sentence with ', p.text_runs[0].text
+    assert_equal 'all', p.text_runs[1].text
+    assert_equal ' formatting options in the middle of the sentence.', p.text_runs[2].text
   end
   
   def test_normal_formatting
-    assert_equal default_formatting, @formatting[0]
-    assert_equal default_formatting, @formatting[4]
+    assert_equal default_formatting, @formatting[0][0]
+    assert_equal default_formatting, @formatting[4][0]
   end
   
   def test_italic_formatting
-    assert_equal only_italic, @formatting[1]
+    assert_equal only_italic, @formatting[1][0]
   end
   
   def test_bold_formatting
-    assert_equal only_bold, @formatting[2]
+    assert_equal only_bold, @formatting[2][0]
   end
   
   def test_underline_formatting
-    assert_equal only_underline, @formatting[3]
+    assert_equal only_underline, @formatting[3][0]
+  end
+  
+  def test_mixed_formatting
+    assert_equal default_formatting,     @formatting[5][0]
+    assert_equal all_formatting_options, @formatting[5][1]
+    assert_equal default_formatting,     @formatting[5][2]
   end
   
   private
@@ -40,26 +56,18 @@ class FormattingTest < Test::Unit::TestCase
   end
   
   def only_italic
-    {
-      italic:    true,
-      bold:      false,
-      underline: false
-    }
+    default_formatting.merge italic: true
   end
   
   def only_bold
-    {
-      italic:    false,
-      bold:      true,
-      underline: false
-    }
+    default_formatting.merge bold: true
   end
   
   def only_underline
-    {
-      italic:    false,
-      bold:      false,
-      underline: true
-    }
+    default_formatting.merge underline: true
+  end
+  
+  def all_formatting_options
+    default_formatting.merge italic: true, bold: true, underline: true
   end
 end
