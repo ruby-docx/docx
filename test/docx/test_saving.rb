45dd37fd47a60a310760e19094e3208acc7e7d73
Christopher Hunt <chrahunt@gmail.com>
2013-06-01 12:07:41 -0400

    Failing tests for using temp file

--- test/docx/test_saving.rb
+++ test/docx/test_saving.rb
@@ -1,5 +1,6 @@
 require 'docx'
 require 'test/unit'
+require 'tempfile'
 
 class SaveTest < Test::Unit::TestCase
   def setup
@@ -11,6 +12,19 @@ class SaveTest < Test::Unit::TestCase
     @doc.save('test/fixtures/new_save.docx')
     @new_doc = Docx::Document.open('test/fixtures/saving.docx')
     assert_equal @doc.paragraphs.size, @new_doc.paragraphs.size
+  end
+
+  def test_saving_to_tempfile
+    temp_file = Tempfile.new(['docx_gem', '.docx'])
+    @new_doc_path = temp_file.path
+    @doc.save(@new_doc_path)
+    @new_doc = Docx::Document.open(@new_doc_path)
+    assert_equal @doc.paragraphs.size, @new_doc.paragraphs.size
+
+    temp_file.close
+    temp_file.unlink
+    # ensure temp file is gone
+    assert equal(nil, File.exists?(@new_doc_path))
   end
 
   def teardown
