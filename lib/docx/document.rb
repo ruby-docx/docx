1b730f72de970b15ead59c51041fdd2ba2d9af16
Aashish Kiran <aashish.kiran@gmail.com>
2017-08-30 03:54:14 +0530

    will able to read header and footer

--- lib/docx/document.rb
+++ lib/docx/document.rb
@@ -18,12 +18,25 @@ module Docx
   #     puts d.text
   #   end
   class Document
-    attr_reader :xml, :doc, :zip, :styles
+    attr_reader :xml, :doc, :zip, :styles, :header, :footer
     
     def initialize(path, &block)
       @replace = {}
       @zip = Zip::File.open(path)
       @document_xml = @zip.read('word/document.xml')
+      header_path = 'word/header1.xml'
+      footer_path = 'word/footer1.xml'
+
+      if @zip.find_entry(header_path)
+        @header_xml = @zip.read(header_path) 
+        @header = Nokogiri::XML(@header_xml)
+      end
+
+      if @zip.find_entry(footer_path)
+        @footer_xml = @zip.read(footer_path)
+        @footer = Nokogiri::XML(@footer_xml)
+      end
+      
       @doc = Nokogiri::XML(@document_xml)
       @styles_xml = @zip.read('word/styles.xml')
       @styles = Nokogiri::XML(@styles_xml)
